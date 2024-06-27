bdc1e03259a26db1526614fd2c2a91e41f4e0e1a
Oleksiy Litvinov <leetwinoff92@gmail.com>
2024-06-27 19:55:02 +0200

    Day 4 - 1 MethodField - get_price

--- server/services/serializers.py
+++ server/services/serializers.py
@@ -14,6 +14,14 @@ class SubscriptionSerializer(serializers.ModelSerializer):
     plan = PlanSerializer()
     clients_name = serializers.CharField(source='client.company_name', read_only=True)
     email = serializers.CharField(source='client.user.email', read_only=True)
+    price = serializers.SerializerMethodField()
+
+    def get_price(self, instance):
+        return (instance.service.full_price - (
+                instance.service.full_price * instance.plan.discount_percent / 100
+        ))
+
+
     class Meta:
         model = Subscription
-        fields = ['id', 'plan_id', 'clients_name', 'email', 'plan']
+        fields = ['id', 'plan_id', 'clients_name', 'email', 'plan', 'price']
