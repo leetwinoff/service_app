6b26215311a00616bab960b34b240c3cbd9606c4
Oleksiy Litvinov <leetwinoff92@gmail.com>
2024-06-27 20:08:38 +0200

    Day 4 - 1 added annotate function in view and redefined list function

--- server/services/serializers.py
+++ server/services/serializers.py
@@ -16,11 +16,8 @@ class SubscriptionSerializer(serializers.ModelSerializer):
     email = serializers.CharField(source='client.user.email', read_only=True)
     price = serializers.SerializerMethodField()
 
-    def get_price(self, instance):
-        return (instance.service.full_price - (
-                instance.service.full_price * instance.plan.discount_percent / 100
-        ))
-
+    def get_price(self, obj):
+        return obj.price
 
     class Meta:
         model = Subscription
--- server/services/views.py
+++ server/services/views.py
@@ -2,14 +2,23 @@ from django.shortcuts import render
 from .models import Subscription
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from .serializers import SubscriptionSerializer
-from django.db.models import Prefetch
+from django.db.models import Prefetch, F
 from clients.models import Client
+from rest_framework.response import Response
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
     queryset = Subscription.objects.all().prefetch_related(
         'plan',
         Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name', 'user__email')),
-    )
+    ).annotate(
+        price=F("service__full_price") - (F("service__full_price") * F("plan__discount_percent") / 100
+                                          ))
     serializer_class = SubscriptionSerializer
 
+
+    def list(self, request, *args, **kwargs):
+        response = super().list(request, *args, **kwargs)
+        response_data = {"result:" : response.data}
+        response = Response(response_data)
+        return response
