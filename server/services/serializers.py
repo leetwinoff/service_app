52b1bd71eb1a150d711f02d3c0a06866535341d8
Oleksiy Litvinov <leetwinoff92@gmail.com>
2024-06-27 19:48:53 +0200

    Day 3 - 1

--- server/services/serializers.py
+++ server/services/serializers.py
@@ -1,10 +1,19 @@
 from rest_framework import serializers
-from .models import Subscription
+from .models import Subscription, Plan
+
+
+class PlanSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Plan
+        fields = ['id', 'plan_type', 'discount_percent']
+
 
 
 class SubscriptionSerializer(serializers.ModelSerializer):
+    plan = PlanSerializer()
     clients_name = serializers.CharField(source='client.company_name', read_only=True)
     email = serializers.CharField(source='client.user.email', read_only=True)
     class Meta:
         model = Subscription
-        fields = ['id', 'plan_id', 'clients_name', 'email']
+        fields = ['id', 'plan_id', 'clients_name', 'email', 'plan']
